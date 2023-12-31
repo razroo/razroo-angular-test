78f3fcd00f6f297f294fedac0911120507947656
Charlie Greenman <charlie@razroo.com>
2023-12-31 14:01:04 -0500

    Add code for angular-17.0.0 integrate-side-nav.
    [Url: razroo.com/community/angular-17.0.0/side-nav-recipe/integrate-side-nav]

--- apps/razroo-angular-test/src/app/app.module.ts
+++ apps/razroo-angular-test/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./routing/app-routing.module";
 import { GlobalHeaderComponentModule } from "@razroo-angular-test/common/ui";
 import { GlobalFooterModule } from "@razroo-angular-test/common/ui";
+import { GlobalSidenavModule } from "@razroo-angular-test/common/ui";
+import { MatListModule } from "@angular/material/list";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, GlobalHeaderComponentModule, GlobalFooterModule, GlobalSidenavModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
