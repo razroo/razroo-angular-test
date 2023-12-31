83853a8b00bb6d9a5a39a5b10d76e74df83c5ae1
Charlie Greenman <charlie@razroo.com>
2023-12-31 14:03:40 -0500

    Add code for angular-17.0.0 use-common-environment.
    [Url: razroo.com/community/angular-17.0.0/add-environment-lib/use-common-environment]

--- apps/razroo-angular-test/src/main.ts
+++ apps/razroo-angular-test/src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { environment } from '@razroo-angular-test/common/common-environment';
 
 if (environment.production) {
   enableProdMode();
